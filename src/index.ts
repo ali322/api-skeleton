53ff644068bb676c553ec88bb8ce26c01e38fb53
alichen <ali322@gmail.com>
2019-02-14 17:33:05 +0800

    add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import * as Koa from 'koa';
+import * as Koa from 'koa'
 import router from './router'
 
-const app = new Koa();
+const app = new Koa()
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 app.listen(3000, () => {
-  console.log('Server running at port 3000');
-});
+  console.log('Server running at port 3000')
+})
--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,7 @@ import * as Router from 'koa-router'
 const router = new Router()
 
 router.get('/', ctx => {
-  ctx.body = 'hello world132'
+  ctx.body = 'hello world!'
 })
 
 export default router
