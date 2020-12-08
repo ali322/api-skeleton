bc6a83f54bafb28d919023bdb537d04ab9b1d6c7
Ali Chen <ali322@gmail.com>
2020-12-08 14:59:51 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import createApp from './app'
 
-const port = process.env.APP_PORT || 5000
 createApp().then((app) => {
+  const port = process.env.APP_PORT || 5000
   app.listen(port, () => {
     console.log("app started at localhost:%d", port)
   })
